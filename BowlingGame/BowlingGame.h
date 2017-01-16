f428ae17b738944248a0445c1c973bad94f24314
Dan Esrey <danesrey@dans-air.lan>
2017-01-16 15:37:42 -0500

    initial commit

---
+++ BowlingGame/BowlingGame.h
@@ -0,0 +1,19 @@
+//
+//  BowlingGame.h
+//  BowlingGame
+//
+//  Created by Dan Esrey on 2017/16/01.
+//  Copyright © 2017 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BowlingGame.
+FOUNDATION_EXPORT double BowlingGameVersionNumber;
+
+//! Project version string for BowlingGame.
+FOUNDATION_EXPORT const unsigned char BowlingGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingGame/PublicHeader.h>
+
+
---
+++ BowlingGameTests/BowlingGameTests.swift
@@ -0,0 +1,69 @@
+//
+//  BowlingGameTests.swift
+//  BowlingGameTests
+//
+//  Created by Dan Esrey on 2017/16/01.
+//  Copyright © 2017 Dan Esrey. All rights reserved.
+//
+
+import XCTest
+@testable import BowlingGame
+
+class BowlingGameTests: XCTestCase {
+    var game: Game!
+    
+    override func setUp() {
+        super.setUp()
+        game = Game()
+    }
+    
+    override func tearDown() {
+        game = nil
+        super.tearDown()
+    }
+    
+    private func rollMany(pins: Int, times: Int) {
+        for _ in 1...times {
+            game.roll(pins)
+        }
+    }
+    
+    func testGutterGame() {
+        rollMany(pins: 0, times: 20)
+        XCTAssertEqual(game.score(), 0)
+    }
+    
+    func testAllOnes() {
+        rollMany(pins: 1, times: 20)
+        XCTAssertEqual(game.score(), 20)
+    }
+    
+    private func rollSpare() {
+        game.roll(5)
+        game.roll(5)
+    }
+    
+    private func rollStrike() {
+        game.roll(10)
+    }
+    
+    func testOneSpare() {
+        rollSpare()
+        game.roll(3)
+        rollMany(pins: 0, times: 17)
+        XCTAssertEqual(game.score(), 16)
+    }
+    
+    func testOnestrike() {
+        rollStrike()
+        game.roll(3)
+        game.roll(4)
+        rollMany(pins: 0, times: 16)
+        XCTAssertEqual(game.score(), 24)
+    }
+    
+    func testPerfectGame() {
+        rollMany(pins: 10, times: 12)
+        XCTAssertEqual(game.score(), 300)
+    }
+}
